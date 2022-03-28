b2e8d4df7d920a81447cb4060cc26a65be3f9de3
Pablo Gainza <pablo.gainza@epfl.ch>
2022-03-28 16:46:52 +0200

    Added model templates to recompute masif seed and masif site without the benchmark proteins.

---
+++ masif/data/masif_ppi_search/nn_models/sc05_seed_benchmark/all_feat/custom_params.py
@@ -0,0 +1,5 @@
+custom_params = {}
+custom_params['cache_dir'] = 'nn_models/sc05_seed_benchmark/cache/'
+custom_params['model_dir'] = 'nn_models/sc05_seed_benchmark/all_feat/model_data/'
+custom_params['desc_dir'] = 'descriptors/sc05_seed_benchmark/all_feat/'
+custom_params['feat_mask'] = [1.0, 1.0, 1.0, 1.0, 1.0]
---
+++ masif/data/masif_site/nn_models/all_feat_3l_masif_seed_benchmark/README.md
@@ -0,0 +1,1 @@
+This neural network was trained specifically for the helices benchmark in the masif_seed paper, excluding all proteins in the benchmark from the training set.
