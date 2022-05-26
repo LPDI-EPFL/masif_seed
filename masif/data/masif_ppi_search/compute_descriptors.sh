b5286393b72e78de7994bba10d6ce6fd66fbde7a
Pablo Gainza <pablo.gainza@epfl.ch>
2022-05-26 08:01:48 +0200

    Updated to seed benchmark.

--- masif/data/masif_ppi_search/compute_descriptors.sh
+++ masif/data/masif_ppi_search/compute_descriptors.sh
@@ -5,7 +5,7 @@
 # ./compute_descriptors.sh {PDBID_CHAIN} 
 # where {PDBID_CHAIN} is something like 4ZQK_A
 masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
-masif_data=$masif_root/data/
+masif_source=$masif_root/masif/source/
+masif_data=$masif_root/masif/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
-python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py nn_models.sc05.all_feat.custom_params $1 $2
+python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py nn_models.sc05_seed_benchmark.all_feat.custom_params $1 $2
