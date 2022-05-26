781aa8ce1ff90cc5911bf8a6f6323e80ddd1b133
Pablo Gainza <pablo.gainza@epfl.ch>
2022-05-26 07:50:59 +0200

    Added the neural network used for the seed benchmark

---
+++ masif/data/masif_site/nn_models/all_feat_3l_seed_benchmark/custom_params.py
@@ -0,0 +1,8 @@
+custom_params = {}
+custom_params['model_dir'] = 'nn_models/all_feat_3l_seed_benchmark/model_data/'
+custom_params['feat_mask'] = [1.0, 1.0, 1.0, 1.0, 1.0]
+custom_params['n_conv_layers'] = 3
+custom_params['out_pred_dir'] = 'output/all_feat_3l_seed_benchmark/pred_data/'
+custom_params['out_surf_dir'] = 'output/all_feat_3l_seed_benchmark/pred_surfaces/'
+custom_params['training_list'] = 'lists/training_seed_benchmark.txt'
+custom_params['testing_list'] = 'lists/testing_seed_benchmark.txt'
