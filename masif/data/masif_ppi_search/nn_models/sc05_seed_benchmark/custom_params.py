ac3af0cc6586e63b695f0cba8fd5aa4129f73847
Pablo Gainza <pablo.gainza@epfl.ch>
2022-05-26 08:02:48 +0200

    Updated custom params for seed benchmark.

--- masif/data/masif_ppi_search/nn_models/sc05_seed_benchmark/custom_params.py
+++ masif/data/masif_ppi_search/nn_models/sc05_seed_benchmark/custom_params.py
@@ -1,7 +1,7 @@
 custom_params = {}
-custom_params['cache_dir'] = 'nn_models/sc05/cache/'
-custom_params['model_dir'] = 'nn_models/sc05/all_feat_seed_benchmark/model_data/'
-custom_params['desc_dir'] = 'output/sc05/all_feat_seed_benchmark/model_data/'
+custom_params['cache_dir'] = 'nn_models/sc05_seed_benchmark/cache/'
+custom_params['model_dir'] = 'nn_models/sc05_seed_benchmark/all_feat/model_data/'
+custom_params['desc_dir'] = 'output/sc05_seed_benchmark/all_feat/model_data/'
 custom_params['gif_eval_out'] = 'nn_models/sc05/gif_eval/'
 custom_params['min_sc_filt'] = 0.5
 custom_params['max_sc_filt'] = 1.0
