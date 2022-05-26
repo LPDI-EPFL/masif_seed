c07e592d1153f984325c16eab3a4e1f3fa2ee277
Pablo Gainza <pablo.gainza@epfl.ch>
2022-05-26 07:52:44 +0200

    Updated files to new version; removed old unused files.

--- masif/data/masif_site/predict_site.sh
+++ masif/data/masif_site/predict_site.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
-masif_matlab=$masif_root/source/matlab_libs/
-masif_data=$masif_root/data/
+masif_source=$masif_root/masif/source/
+masif_data=$masif_root/masif/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python -W ignore $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l.custom_params $1 $2
+python -W ignore $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l_seed_benchmark.custom_params $1 $2
--- masif/data/masif_site/train_nn.sh
+++ masif/data/masif_site/train_nn.sh
@@ -1,5 +1,5 @@
 masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
-masif_data=$masif_root/data/
+masif_source=$masif_root/masif/source/
+masif_data=$masif_root/masif/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python3 $masif_source/masif_site/masif_site_train.py $1
+python3 -W ignore $masif_source/masif_site/masif_site_train.py $1
