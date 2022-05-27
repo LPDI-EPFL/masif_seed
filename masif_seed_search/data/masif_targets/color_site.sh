48d62567968237df9f21041a9e808ddd3ac6b172
Pablo Gainza <pablo.gainza@epfl.ch>
2022-05-27 07:45:01 +0200

    Added main masif-seed target directory

---
+++ masif_seed_search/data/masif_targets/color_site.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+masif_root=../../../
+masif_source=$masif_root/source/
+masif_data=$masif_root/data/
+export PYTHONPATH=$masif_source:$masif_data/masif_site/
+python -W ignore $masif_source/masif_site/masif_site_label_surface.py nn_models.all_feat_3l.custom_params $1 $2
---
+++ masif_seed_search/data/masif_targets/targets/template/run_reeval.sh
@@ -0,0 +1,11 @@
+masif_root=$(git rev-parse --show-toplevel)
+export masif_db_root=/home/gainza/lpdi_fs/masif/
+masif_source=$masif_root/source/
+masif_matlab=$masif_root/source/matlab_libs/
+masif_data=$masif_root/data/
+export masif_root
+export PYTHONPATH=$PYTHONPATH:$masif_source:`pwd`
+#python $masif_source/masif_seed_search/masif_seed_search_nn.py params 4ZQK_A
+#python $masif_source/masif_seed_search/masif_seed_search_nn.py params_small_proteins 5JDS_A
+python $masif_source/masif_seed_search/masif_seed_search_nn.py params_small_proteins_reeval 4ZQK_A
+#python $masif_source/masif_seed_search/masif_seed_search_nn.py params_peptides 4ZQK_A
