60e7392478fc74c1c2a1d1f9677cfcf4895c03da
Anthony Marchand <43955752+anthomarchand@users.noreply.github.com>
2022-06-16 10:13:40 +0200

    Create seednum.sh

---
+++ rosetta_scripts/tools/seednum.sh
@@ -0,0 +1,9 @@
+# This file is adding a unique seed number ('_S[ID]') to each input PDB containing a seed in complex with the target
+
+i=1
+for file in $(ls ./input/*.pdb)
+  do  base=$(basename $file .pdb)
+      mv $file $base\_S$i.pdb
+      i=`expr $i + 1`
+      echo $i
+  done
