d8f9a2443530342a2428be40f5f2f2eeea9e1e5c
Pablo Gainza <pablo.gainza@epfl.ch>
2023-04-26 16:03:18 +0200

    Update data_prepare_one.sh

--- masif/data/masif_site/data_prepare_one.sh
+++ masif/data/masif_site/data_prepare_one.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
-masif_matlab=$masif_root/source/matlab_libs/
+masif_source=$masif_root/masif/source/
 export PYTHONPATH=$PYTHONPATH:$masif_source
-export masif_matlab
 if [ "$1" == "--file" ]
 then
 	echo "Running masif site on $2"
