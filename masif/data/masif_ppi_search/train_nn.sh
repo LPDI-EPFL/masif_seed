c18c69beb64828fb6e93ffc31a10fe0dd06066ab
Pablo Gainza <pablo.gainza@epfl.ch>
2023-04-26 16:02:30 +0200

    Update train_nn.sh

--- masif/data/masif_ppi_search/train_nn.sh
+++ masif/data/masif_ppi_search/train_nn.sh
@@ -1,5 +1,5 @@
 masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/masif/source/
-masif_data=$masif_root/data/
+masif_data=$masif_root/masif/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
 python $masif_source/masif_ppi_search/masif_ppi_search_train.py $1
